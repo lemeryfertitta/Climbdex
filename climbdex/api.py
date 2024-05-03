8593fd1d66a93776e6286d795348c5daa58f0f39
Joshua Burman <jburman@meazurelearning.com>
2024-05-03 15:59:17 -0400

    finished adding all db to api error handling

--- climbdex/api.py
+++ climbdex/api.py
@@ -23,23 +23,32 @@ def error_handler(err):
 
 @blueprint.route("/api/v1/<board_name>/layouts")
 def layouts(board_name):
-    return flask.jsonify(climbdex.db.get_data(board_name, "layouts"))
+    try:
+        return flask.jsonify(climbdex.db.get_data(board_name, "layouts"))
+    except Exception as e:
+        return error_handler(e)
 
 
 @blueprint.route("/api/v1/<board_name>/layouts/<layout_id>/sizes")
 def sizes(board_name, layout_id):
-    return flask.jsonify(
-        climbdex.db.get_data(board_name, "sizes", {"layout_id": int(layout_id)})
-    )
+    try:
+        return flask.jsonify(
+            climbdex.db.get_data(board_name, "sizes", {"layout_id": int(layout_id)})
+        )
+    except Exception as e:
+        return error_handler(e)
 
 
 @blueprint.route("/api/v1/<board_name>/layouts/<layout_id>/sizes/<size_id>/sets")
 def sets(board_name, layout_id, size_id):
-    return flask.jsonify(
-        climbdex.db.get_data(
-            board_name, "sets", {"layout_id": int(layout_id), "size_id": int(size_id)}
+    try:
+        return flask.jsonify(
+            climbdex.db.get_data(
+                board_name, "sets", {"layout_id": int(layout_id), "size_id": int(size_id)}
+            )
         )
-    )
+    except Exception as e:
+        return error_handler(e)
 
 
 @blueprint.route("/api/v1/search/count")
@@ -77,7 +86,10 @@ def search(
 
 @blueprint.route("/api/v1/<board_name>/beta/<uuid>")
 def beta(board_name, uuid):
-    return flask.jsonify(climbdex.db.get_data(board_name, "beta", {"uuid": uuid}))
+    try:
+        return flask.jsonify(climbdex.db.get_data(board_name, "beta", {"uuid": uuid}))
+    except Exception as e:
+        return error_handler(e)
 
 
 @blueprint.route("/api/v1/login/", methods=["POST"])
